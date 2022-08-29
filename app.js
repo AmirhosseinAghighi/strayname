dd37d7226860e1c0dfe5f7cff6a2895417fbc06e
DuckWichTrust <68964814+AmirhosseinAghighi@users.noreply.github.com>
2022-08-29 22:59:07 +0430

    Update app.js
    only english update ...

--- app.js
+++ app.js
@@ -70,3 +70,12 @@ function create() {
     $("#output").fadeIn();
   }
 }
+
+$("#input").keypress(function (event) {
+  var ew = event.which;
+  if (ew == 32) return true;
+  if (48 <= ew && ew <= 57) return true;
+  if (65 <= ew && ew <= 90) return true;
+  if (97 <= ew && ew <= 122) return true;
+  return false;
+});
